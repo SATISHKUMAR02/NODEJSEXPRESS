4c0ace83079edad1ef03d883d0b1da83fb2761ce
SATISH <satishk02122003@gmail.com>
2025-06-24 12:37:29 +0530

    completed creating a server

---
+++ server1.js
@@ -0,0 +1,13 @@
+// console.log(global);
+// const os = require('os');
+// const path = require('path');
+// console.log(os.type())
+// console.log(os);
+// console.log(path);
+
+// console.log(path.parse(__filename));
+// // const add = (a,b)=>a+b;  this is how we can export functions 
+// // const sub = (a,b)=>a-b;  to different files 
+// // const div = (a,b)=>a/b; 
+// // const mul = (a,b)=>a*b; 
+// // module.exports = {add,sub,div,mul}
